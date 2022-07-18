1ac4ce5e7b3d47fe7d812d36cef67024b54a87b6
Rajesh Sirohi <rsirohikoviya@yahoo.com>
2022-07-17 19:27:11 -0700

    intitial changes for FB1

---
+++ practice-git1/src/FB1Test.java
@@ -0,0 +1,5 @@
+
+public class FB1Test {
+	//FB1-sts-new class
+
+}
--- practice-git1/src/FBTest1.java
+++ practice-git1/src/FBTest1.java
@@ -3,6 +3,10 @@ public class FBTest1 {
   
   //comment1 on git on branch spring3
   // comment2 on git on branch sprint3
+
   //sprint2-git-comment1
+
+	//FB1-sts-comment1
+
 
 }
