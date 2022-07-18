bbfaf09934080f2d3802e0d8ad62afe0d2bc3ee2
raj-sirohi <rsirohikoviya@yahoo.com>
2022-07-17 19:07:35 -0700

    initial master

--- practice-git1/src/Test1.java
+++ practice-git1/src/Test1.java
@@ -1,30 +1,6 @@
 
 public class Test1 {
 	
-	// comment1
-
-	// comment2
 	
-	// comment3
-	
-	// spring1 comments //
-	// spring1
-	// spring 1-1
-	//sprint 1-2 in editor
-	
-
-	// sprint2
-	
-
-	// on git - sprint 1-2
-	// on editor spring 1-3
-	
-	// on git sprint 1-3
-
-	// added on git
-
-	// comment 1 on STSS
-
-
 	
 }
